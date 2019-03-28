4e20d1085d9e794cb345359b3312ac49325a43a1
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-03-28 14:20:56 -0700

    plugin-plugin 9.3.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
 plugins {
     `kotlin-dsl`
-    id("nebula.plugin-plugin") version "9.3.4"
+    id("nebula.plugin-plugin") version "9.3.7"
 }
 
 description = "Additional Bintray tasks"
